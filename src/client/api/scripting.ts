4de4195c32c4478bcc7d7638a41ff5544b4ac024
cohenerickson <cohenerickson@gmail.com>
2024-01-20 11:37:58 -0600

    web workers

--- src/client/api/scripting.ts
+++ src/client/api/scripting.ts
@@ -10,6 +10,24 @@ window.Function = new Proxy(Function, {
   }
 });
 
+window.Worker = new Proxy(Worker, {
+  construct(target, args) {
+    if (args[0]) args[0] = __$ampere.rewriteJS(args[0], __$ampere.base);
+
+    return Reflect.construct(target, args);
+  }
+});
+
+if ("imoportScripts" in self) {
+  self.importScripts = new Proxy(self.importScripts, {
+    apply(target, thisArg, args) {
+      args = args.map((arg) => __$ampere.rewriteURL(arg, __$ampere.base));
+
+      return Reflect.apply(target, thisArg, args);
+    }
+  });
+}
+
 export function createEvalProxy(evalFunction: (script: string) => any): any {
   new Proxy(evalFunction, {
     apply(target, thisArg, args) {
